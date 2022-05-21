290225c3c7f0089023dacf2052d19b7997a92618
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-05-21 14:40:04 +0300

    fix: fix with Eslint (Sat, May 21, 2022 02:40:04 PM)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-let { stdout, stdin, exit } = require('process');
+let { stdout, stdin } = require('process');
 
-const buyFunc = () => {
-    stdout.write('Good luck! I hope see you again.');
-    exit();
-}
+const byeFunc = () => {
+  stdout.write('Good luck! I hope see you again.');
+  process.exit();
+};
 
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 stdout.write('Hello! I need some text from you..Do it:\n');
 stdin.on('data', (data) => {
-    if (data.toString().trim() === 'exit') {
-        buyFunc()
-    }
-    output.write(data);
-})
-process.on('SIGINT', buyFunc);
-
-
+  if (data.toString().trim() === 'exit') {
+    byeFunc();
+  }
+  output.write(data);
+});
+process.on('SIGINT', byeFunc);
