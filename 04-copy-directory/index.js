d6f4888e01e8e68bccb08fdc8456451392c79295
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-05-22 23:21:18 +0300

    feat: solve 04-copy-directory (Sun, May 22, 2022 11:21:17 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+let path = require('path');
+let fs = require('fs/promises');
+let folder = path.join(__dirname, 'files');
+let folderCopy = path.join(__dirname, 'files-copy');
 
+fs.rm(folderCopy, { recursive: true, force: true }).finally(() => {
+  fs.mkdir(folderCopy, { recursive: true });
+  fs.readdir(folder, { withFileTypes: true }).then((data) => {
+    data.forEach((file) => {
+      if (file.isFile()) {
+        let filePathOld = path.join(folder, file.name);
+        let filePathNew = path.join(folderCopy, file.name);
+        fs.copyFile(filePathOld, filePathNew);
+      }
+    });
+  });
+});
