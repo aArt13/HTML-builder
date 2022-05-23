1900fa5770f99816045312f72783fda807b51e82
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-05-23 23:02:04 +0300

    fix: fix 05-merge-styles (Mon, May 23, 2022 11:02:03 PM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,6 +5,9 @@ let bundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 fs.readdir(input, 'utf-8', (error, files) => {
   if (error) return console.log(error.message); 
+  fs.writeFile(bundle, '', (error) => {
+    if (error) return console.log(error.message); 
+  });
   files.forEach((file) => {
     if (path.parse(path.join(input, file)).ext === '.css') {
       let stream = fs.createReadStream(path.join(input, file));
