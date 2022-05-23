683909edae615928af6902216bb7cab382722a5b
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-05-23 22:50:11 +0300

    feat: solve 05-merge-styles (Mon, May 23, 2022 10:50:11 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-// const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
-const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+// const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 let data = '';
 stream.on('data', chunk => data += chunk);
 stream.on('end', () => console.log(data));
 stream.on('error', error => console.log('Error: ', error.message));
+
+// fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (error, data) => {
+//  if (error) throw error;
+//  console.log(data);
+//});
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,18 @@
+let path = require('path');
+let fs = require('fs');
+let input = path.join(__dirname, 'styles');
+let bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+
+fs.readdir(input, 'utf-8', (error, files) => {
+  if (error) return console.log(error.message); 
+  files.forEach((file) => {
+    if (path.parse(path.join(input, file)).ext === '.css') {
+      let stream = fs.createReadStream(path.join(input, file));
+      stream.on('data', (data) => {
+        fs.appendFile(bundle, data, (error) => {
+          if (error) return console.log(error.message); 
+        });
+      });
+    }
+  });
+});
